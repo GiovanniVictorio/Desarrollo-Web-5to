825f119ea93ecd52e41d0d10d1966c5b2ca6faf3
Giovanni <giovanni.vic99@gmail.com>
2020-10-15 20:30:51 -0600

    Add Ejercicio 3

---
+++ Javascript/Ejercicio 3/js/java.js
@@ -0,0 +1,5 @@
+var mensaje = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+
+for ( var x = 0; x < 12; x++ ) {
+	alert( mensaje[x] )
+}
